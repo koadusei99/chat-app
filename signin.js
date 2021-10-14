5a691f516adba433a1c29f54659cd2812c31ed4e
Maampeaw <mamoah014@st.ug.edu.gh>
2021-10-14 02:19:10 -1200

    added form validation and  sign in

---
+++ signin.js
@@ -0,0 +1,37 @@
+//get all relevant elements from DOM
+const usernameInput = document.getElementById('username');
+const passwordInput = document.getElementById('password');
+const button = document.getElementById('submitButton');
+
+// get user name and password
+
+
+// validate credentials
+function checkInput(){
+    // get user name and password
+    let username = usernameInput.value;
+    let password = passwordInput.value;
+    // console.log(username)
+    // console.log(password)
+    if (username.length<6){
+        console.log('Invalid username');
+        return
+    }
+    if(password.length<8){
+        console.log('invalid password');
+        return 
+    }
+    console.log('Signed in');
+    // store username in localstorage
+    localStorage.setItem('username', username);
+
+    // redirect to chat page
+    window.location.href= 'index.html'
+    
+    return 
+    
+}
+
+
+// redirect to chat page
+button.addEventListener('click', checkInput)
